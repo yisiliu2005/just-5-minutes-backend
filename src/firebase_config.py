9dcc208bee8b0223c3891cd31cde89ed6c200714
Yisi Liu <liu1857@mcmaster.ca>
2025-10-25 15:31:41 -0400

    firebase config

--- src/firebase_config.py
+++ src/firebase_config.py
@@ -1,4 +1,11 @@
 # This file contains the firebase configuration details.
+# For Firebase JS SDK v7.20.0 and later, measurementId is optional
 firebaseConfig = {
-
+    "apiKey": "AIzaSyB16xJl6gKNFuhbSVCgdcjQdzSNTRQZtys",
+    "authDomain": "just-5-minutes-41bc8.firebaseapp.com",
+    "projectId": "just-5-minutes-41bc8",
+    "storageBucket": "just-5-minutes-41bc8.firebasestorage.app",
+    "messagingSenderId": "961449515029",
+    "appId": "1:961449515029:web:55901b44437cf91947561c",
+    "measurementId": "G-RYV233W2M4"
 }
