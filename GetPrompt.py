79370b56253ae6a09422269e788af78968964e5a
Vishva Sritharan <66765929+vishva12124@users.noreply.github.com>
2025-10-25 21:35:34 -0400

    updated gemini prompt teehee

--- GetPrompt.py
+++ GetPrompt.py
@@ -2,6 +2,13 @@ from dotenv import load_dotenv
 import os
 from flask import Flask, request
 from google import genai
+from pydantic import BaseModel
+
+
+class StoreData(BaseModel):
+    title: str
+    startTime: str
+    endTime: str
 
 load_dotenv()
 
@@ -15,8 +22,12 @@ def getGeminiResponses():
     if request.is_json:
         data = request.get_json(force=True)
         response = client.models.generate_content(
-            model="gemini-2.5-flash", 
-            contents=(str(data) + prompt)
+            model="gemini-2.5-flash",
+            contents= (str(data), " , please create a time for these habits during the day. It shold be in this format: title, startTime, endTime. Space the habits out throughout the day; do not have them right next to each other. The end time should be 5 minutes after the start time pretty princess"),
+            config={
+                "response_mime_type": "application/json",
+                "response_schema": list[StoreData],
+            },
         )
     return response.text, 200
 
